8f58c5f5bef78b7b9f2926259cd127af0251d3bb
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-11-15 01:12:36 -0600

    search bar functioning!

--- src/App.js
+++ src/App.js
@@ -3,11 +3,72 @@ import ContactList from './ContactList';
 import SearchBar from './SearchBar';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      contacts: [
+        {
+          id: 'dale',
+          name: 'Dale Cooper',
+          occupation: 'FBI Agent',
+          avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
+        },
+        {
+          id: 'spike',
+          name: 'Spike Spiegel',
+          occupation: 'Bounty Hunter',
+          avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
+        },
+        {
+          id: 'wirt',
+          name: 'Wirt',
+          occupation: 'Adventurer',
+          avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
+        },
+        {
+          id: 'michael',
+          name: 'Michael',
+          occupation: 'Loving little brother',
+          avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
+        },
+        {
+          id: 'dana',
+          name: 'Dana Scully',
+          occupation: 'FBI Agent',
+          avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
+        }
+      ]
+    };
+  }
+  handleSearchBarChange(event) {
+    this.setState({
+      contacts: this.state.contacts,
+      searchText: event.target.value
+    })
+  }
+
+  getFilteredContacts() {
+    //remove whitespace and make lowercase
+    const term = this.state.searchText.trim().toLowerCase();
+    const contacts = this.state.contacts;
+
+    //if term is empty string, return all contacts
+    if (!term) {
+      return contacts;
+    }
+
+    //return contacts where search term is true
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().search(term) >= 0;
+    });
+  }
+render() {
     return (
       <div className="App">
-        <SearchBar />
-        <ContactList />
+        <SearchBar value={this.state.searchText} onChange={this.handleSearchBarChange.bind(this)}/>
+        <ContactList contacts={this.getFilteredContacts()}/>
       </div>
     );
   }
--- src/ContactList.js
+++ src/ContactList.js
@@ -2,59 +2,21 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class ContactList extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      contacts: [
-        {
-          id: 'dale',
-          name: 'Dale Cooper',
-          occupation: 'FBI Agent',
-          avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
-        },
-        {
-          id: 'spike',
-          name: 'Spike Spiegel',
-          occupation: 'Bounty Hunter',
-          avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
-        },
-        {
-          id: 'wirt',
-          name: 'Wirt',
-          occupation: 'Adventurer',
-          avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
-        },
-        {
-          id: 'michael',
-          name: 'Michael',
-          occupation: 'Loving little brother',
-          avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
-        },
-        {
-          id: 'dana',
-          name: 'Dana Scully',
-          occupation: 'FBI Agent',
-          avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
-        }
-      ]
-    };
-  }
-
-render() {
-  return (
-    <ul className="contact-list">
-      {this.state.contacts.map(contact => {
-        return (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            avatar={contact.avatar}
-            occupation={contact.occupation}
-          />
-        )
-      })}
-    </ul>
+  render() {
+    return (
+      <ul className="contact-list">
+        {this.props.contacts.map(contact => {
+          return (
+            <Contact
+              key={contact.id}
+              name={contact.name}
+              avatar={contact.avatar}
+              occupation={contact.occupation}
+            />
+          )
+        })}
+      </ul>
     );
   }
 }
